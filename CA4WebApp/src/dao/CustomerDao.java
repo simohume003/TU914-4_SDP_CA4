dd61aac2a34d696890ce588e3f04be56114e8d85
simohume003 <simonhume61@gmail.com>
2026-04-15 10:00:02 +0100

    Customer Entity & DAO for DB

---
+++ CA4WebApp/src/dao/CustomerDao.java
@@ -0,0 +1,54 @@
+package dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import entities.Customer;
+
+public class CustomerDao {
+
+    protected static EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("jpaPU");
+
+    public CustomerDao() {
+    }
+
+    public void persist(Customer customer) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(customer);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public Customer getCustomerByEmail(String email) {
+        EntityManager em = emf.createEntityManager();
+        List<Customer> customers = em.createQuery(
+                "SELECT c FROM Customer c WHERE c.email = :email", Customer.class)
+                .setParameter("email", email)
+                .getResultList();
+        em.close();
+
+        if (customers.isEmpty()) {
+            return null;
+        }
+        return customers.get(0);
+    }
+
+    public Customer getCustomerByEmailAndPassword(String email, String password) {
+        EntityManager em = emf.createEntityManager();
+        List<Customer> customers = em.createQuery(
+                "SELECT c FROM Customer c WHERE c.email = :email AND c.password = :password", Customer.class)
+                .setParameter("email", email)
+                .setParameter("password", password)
+                .getResultList();
+        em.close();
+
+        if (customers.isEmpty()) {
+            return null;
+        }
+        return customers.get(0);
+    }
+}
---
+++ CA4WebApp/src/entities/Customer.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+@NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c")
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public Customer() {
+    }
+
+    public Customer(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
